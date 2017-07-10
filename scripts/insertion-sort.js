1c8c1b7ad406c1442e7c2b6649956bcabcf7f87d
David Becker <david@beckersite.com>
2017-07-10 16:48:43 -0600

    Complete Insertion Sort.js file

---
+++ scripts/insertion-sort.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+	for (let i = 1; i < array.length; i++) {
+		let j = i - 1
+		while (array[i] < array[j]) {
+				[array[j] , array[i]] = [array[i], array[j]]
+				i -= 1
+				j = i - 1
+			}
+		}
+	return array
+}
+
+console.log(insertionSort(arraySort2))
