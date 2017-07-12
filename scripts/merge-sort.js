189426ea27581bccf2546f25ad55597d630f5dd8
David Becker <david@beckersite.com>
2017-07-11 19:34:18 -0600

    Bubble-sort testing almost complete

--- scripts/merge-sort.js
+++ scripts/merge-sort.js
@@ -12,15 +12,19 @@ const mergeArrays = (array1, array2) => {
 }
 
 const mergeSort = (array) => {
-	let array1 = []
-	let array2 = []
-	if (array.length === 1) {
-		return array
-	} else if (array.length > 1) {
-		array1 = array.splice(0, Math.floor(array.length/2))
-		array2 = array.splice(0, array.length)
-		return mergeArrays(mergeSort(array1), mergeSort(array2))
-	}
+	if (Array.isArray(array)) {
+    let array1 = []
+  	let array2 = []
+  	if (array.length === 1) {
+  		return array
+  	} else if (array.length > 1) {
+  		array1 = array.splice(0, Math.floor(array.length/2))
+  		array2 = array.splice(0, array.length)
+  		return mergeArrays(mergeSort(array1), mergeSort(array2))
+  	}
+  } else {
+    return `${array} is not an Array!`
+  }
 }
 
 module.exports = mergeSort
--- tests/bubble-sort-test.js
+++ tests/bubble-sort-test.js
@@ -2,18 +2,54 @@ var { expect } = require('chai');
 var bubblesSort = require('../scripts/bubble-sort.js');
 var generateArray = require('../scripts/generateArray.js')
 
-describe('BubbleSort Fun Time', function() {
-  let sortArray;
+describe('BubbleSort Fun Time', () => {
 
-  beforeEach(function() {
-    sortArray = generateArray(10)
-  })
+  it('should be an array', () => {
+    let sortArray = generateArray(10)
 
-  it('should be an array', function () {
     expect(sortArray).to.be.an('array')
   })
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(bubblesSort).to.be.function
+  })
+
+  it('should only accept arrays', () => {
+    expect(bubblesSort('string')).to.equal('string is not an Array!')
+  })
+
+  it('should accept an array of any length', () => {
+    let sortArray = generateArray(10)
+    let sortedArray = bubblesSort(sortArray)
+
+    expect(sortArray).to.equal(sortArray)
+    expect(bubblesSort(sortArray)).to.equal(sortedArray)
+  })
+
+  it('should sort an array of numbers', () => {
+    let sortArrayNums = generateArray(5)
+    let sortedArrayNums = bubblesSort(sortArrayNums)
+
+    expect(sortArrayNums).to.equal(sortArrayNums)
+    expect(bubblesSort(sortArrayNums)).to.equal(sortedArrayNums)
+  })
+
+  it('should sort an array of letters', () => {
+    let sortArrayLet = ['a', 'c', 'd', 'b']
+    let sortedArrayLet = bubblesSort(sortArrayLet)
+
+    expect(sortArrayLet).to.equal(sortArrayLet)
+    expect(bubblesSort(sortArrayLet)).to.equal(sortedArrayLet)
+  })
+
+  it('should not accept an array of numbers and letters')
+
+  it('positive numbers')
+
+  it('negative numbers')
+
+  it('should have the same elements for unsorted and sorted', () => {
+    expect([1, 2, 3]).to.include.members([1, 2]);
+    expect([1, 2, 3]).to.not.have.members([1, 2]);
   })
 })
